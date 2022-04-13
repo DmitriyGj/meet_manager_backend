94d4997f5cd13ba99d96a00329294a11c1c3725c
DmitriyGj <loger865@yandex.ru>
2022-04-13 16:43:07 +0500

    asd

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -3,7 +3,7 @@ require('dotenv').config()
 module.exports = {
     Url: process.env.DB_URL,
     Port: process.env.DB_PORT,
-    Pass: process.env.DB_PASSWORD.toString(),
+    Pass: process.env.DB_PASSWORD,
     User: process.env.DB_USER,
     Name: process.env.DB
 };
