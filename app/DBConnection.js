b912f9065947c9369b7cb4545cda316de64154c0
DmitriyGj <loger865@yandex.ru>
2022-04-13 17:22:26 +0500

    weg

--- app/DBConnection.js
+++ app/DBConnection.js
@@ -1,20 +1,18 @@
 const db_config = require('./config/db.config');
 const knex = require('knex');
 
-const params = db_config.Url.split(':')
 const db = knex({
     client: 'pg',
-    connection:`${db_config.Url}&useSSL=false`
-    // {
-    //     host: db_config.Url,
-    //     user: db_config.User,
-    //     port: db_config.Port,
-    //     password: db_config.Pass,
-    //     database: db_config.Name,
-    //     ssl: {
-    //         rejectUnauthorized: false,
-    //     }
-    // }
+    connection: {
+        host: db_config.Url,
+        user: db_config.User,
+        port: db_config.Port,
+        password: db_config.Pass,
+        database: db_config.Name,
+        ssl: {
+            rejectUnauthorized: false,
+        }
+    }
 });
 
 
--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 
 module.exports = {
-    Url: process.env.DATABASE_URL,
+    Url: process.env.DB_URL,
     Port: process.env.DB_PORT,
     Pass: process.env.DB_PASSWORD,
     User: process.env.DB_USER,
