18a15772205c05793ab51297ca6da83e61864007
DmitriyGj <71816721+DmitriyGj@users.noreply.github.com>
2022-05-13 02:54:16 +0500

    added new router

--- server.js
+++ server.js
@@ -14,14 +14,14 @@ const departamentRouter = require('./app/route/departament.routes');
 const postsRouter = require('./app/route/post.routes');
 const authRouter = require('./app/route/auth.routes')
 const meetingsRouter = require('./app/route/meetings.routes');
-const memberCardsRouter = require('./app/route/memberCards.routes');
+const rolesRouter = require('./app/route/roles.routes');
 
 app.use('/api',employesRouter );
 app.use('/api',departamentRouter);
 app.use('/api',postsRouter);
 app.use('/api', authRouter );
 app.use('/api', meetingsRouter);
-app.use('/app', memberCardsRouter);
+app.use('/api', rolesRouter);
 
 const port = appConfig.Port || 8000;
 app.listen(port, () => console.log(`Сервер работает на порту ${port}, http://localhost:${port}`));
